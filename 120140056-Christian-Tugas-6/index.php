f6eb4f801196f6e41b03ddb50912aba54755934c
120140056 <103248575+120140056@users.noreply.github.com>
2022-11-23 17:26:11 +0700

    tugas 6

--- 120140056-Christian-Tugas-6/index.php
+++ 120140056-Christian-Tugas-6/index.php
@@ -1,31 +1,33 @@
 <?php
-function intToRoman($integer){
-    $integer = intval($integer);
-    $result = '';
- 
-    $lookup = array(
-        'M' => 1000,
-        'CM' => 900,
-        'D' => 500,
-        'CD' => 400,
-        'C' => 100,
-        'XC' => 90,
-        'L' => 50,
-        'XL' => 40,
-        'X' => 10,
-        'IX' => 9,
-        'V' => 5,
-        'IV' => 4,
-        'I' => 1
+
+function intToRoman($x){
+    $x = intval($x);
+    $hasil = '';
+    $array = array(
+        array('M', 1000),
+        array('CM', 900),
+        array('D', 500),
+        array('CD', 400),
+        array('C', 100),
+        array('XC', 90),
+        array('L', 50),
+        array('XL', 40),
+        array('X', 10),
+        array('IX', 9),
+        array('V', 5),
+        array('IV', 4),
+        array('I', 1)
     );
- 
-    foreach($lookup as $roman => $value){
-        $matches = intval($integer/$value);
-        $result .= str_repeat($roman,$matches);
-        $integer = $integer % $value;
+
+    for ($i = 0; $i <= 12; $i++){
+        $temp = intval($x/$array[$i][1]);
+        for($j = 0; $j < $temp; $j++){
+            $hasil .= $array[$i][0];
+        }
+        $x %= $array[$i][1];
     }
  
-    return $result;
+    return $hasil;
 }
 
 echo intToRoman(58);
